18a98b42a2c70f63f4d7d628ed23b0ccd8c9f8a3
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-09-25 22:48:35 +0530

    changes

--- src/Components/ToDo/ToDo.jsx
+++ src/Components/ToDo/ToDo.jsx
@@ -1,7 +1,13 @@
-function Todo({todoData, isFinished}){
+import { useState } from "react"
+
+function Todo({todoData, isFinished, changeFinished}){
+    const [Finished, setFinished] = useState(isFinished);
  return (
     <div>
-        <input type="checkbox" checked={isFinished} onChange={(e)=>console.log(e.target.value)}/>
+        <input type="checkbox" checked={Finished} onChange={(e)=>{
+                                                                    setFinished(e.target.checked)
+                                                                   changeFinished(e.target.checked)
+                                                                  } }/>
         {todoData}
         <button>Edit</button>
         <button>Delete</button>
--- src/Components/ToDoList/ToDoList.jsx
+++ src/Components/ToDoList/ToDoList.jsx
@@ -1,11 +1,22 @@
 import Todo from "../ToDo/ToDo.jsx";
 
-function ToDoList({list}){
+function ToDoList({list, updateList}){
     
         
     return(
     <div>
-    {list.length>0 && list.map(todo=> <Todo key={todo.id}  todoData={todo.todoData} isFinished={todo.finished}/>)}
+    {list.length>0 && list.map((todo)=> <Todo key={todo.id} 
+                                            todoData={todo.todoData} 
+                                            isFinished={todo.finishedo} 
+                                            changeFinished={isFinished=>{
+                                           const updatedList = list.map((t)=> {
+                                                       if(t.id == todo.id){
+                                                          todo.finished = isFinished
+                                                            }
+                                                                return t})
+
+                                                      updateList(updatedList)
+    }}/>)}
     </div>
   )
 }
